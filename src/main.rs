c186ff2c6dc845c7bd4bc813d801b689d5b3df53
MarcosRSouza <marcosmrodrigues.souza@gmail.com>
2023-08-08 11:43:27 -0300

    feat: output elapsed seconds on same line

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::io;
+use std::io::Write;
 use std::thread::sleep;
 use std::time::Duration;
 
 fn focus_time_loop(session_time_in_seconds: u16) -> () {
     for i in 0..session_time_in_seconds {
-        println!("Looping every second: {}", i);
+        print!("\rtime: {}", i);
+        io::stdout().flush().unwrap();
         sleep(Duration::from_secs(1));
     }
 }
@@ -22,10 +24,9 @@ fn main() {
         .read_line(&mut mode)
         .expect("Failed to read line");
 
-    println!("Choosed mode: {mode}");
-
     if mode == "1\n" {
         let session_time_in_seconds = 5 * 1; // TODO: change this time to actual 25 * 60 seconds
         focus_time_loop(session_time_in_seconds);
     }
+    println!();
 }
