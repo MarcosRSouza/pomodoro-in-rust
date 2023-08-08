efab1e3b1a1dae9407f9c4665e045b875e5e1165
MarcosRSouza <marcosmrodrigues.souza@gmail.com>
2023-08-08 14:38:42 -0300

    feat: add customizable session time lengths option

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ use std::io::Write;
 use std::thread::sleep;
 use std::time::Duration;
 
+fn handle_number_input() -> u16 {
+    let mut input_line = String::new();
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+    let parsed_input_number: u16 = input_line.trim().parse().expect("Input is not valid");
+
+    return parsed_input_number;
+}
+
 fn focus_time(minutes: u16) {
     let focus_session_time_in_seconds = minutes * 60;
     let focus_time_message: &str = "Time to focus";
@@ -15,9 +25,16 @@ fn short_break_time(minutes: u16) {
     session_loop(focus_session_time_in_seconds, focus_time_message);
 }
 
+fn long_break_time(minutes: u16) {
+    let focus_session_time_in_seconds = minutes * 60;
+    let focus_time_message: &str = "Take a break ";
+    session_loop(focus_session_time_in_seconds, focus_time_message);
+}
+
 fn default_pomodoro_loop() {
     let focus_session_minutes: u16 = 25;
     let short_break_session_minutes: u16 = 5;
+    let long_break_session_minutes: u16 = 30;
     focus_time(focus_session_minutes);
     short_break_time(short_break_session_minutes);
     focus_time(focus_session_minutes);
@@ -25,6 +42,28 @@ fn default_pomodoro_loop() {
     focus_time(focus_session_minutes);
     short_break_time(short_break_session_minutes);
     focus_time(focus_session_minutes);
+    long_break_time(long_break_session_minutes);
+}
+
+fn customized_session_loop(
+    customized_focus_session_minutes: u16, 
+    customized_short_break_session_minutes: u16,
+    customized_long_break_session_minutes: u16,
+    mut customized_number_of_sections_before_long_break: u16,
+) {
+    while customized_number_of_sections_before_long_break > 0 {
+        focus_time(customized_focus_session_minutes);
+
+        if customized_number_of_sections_before_long_break > 1 {
+            short_break_time(customized_short_break_session_minutes);
+        }
+
+        else if customized_number_of_sections_before_long_break == 1 {
+            long_break_time(customized_long_break_session_minutes);
+        }
+
+        customized_number_of_sections_before_long_break -= 1;
+    }
 }
 
 fn session_loop(session_time_in_seconds: u16, msg: &str) -> () {
@@ -52,8 +91,27 @@ fn main() {
         .read_line(&mut mode)
         .expect("Failed to read line");
 
-    if mode == "1\n" {
+    if mode == "1\n" || mode == "\n" {
         default_pomodoro_loop();
     }
+
+    else if mode == "2\n" {
+        println!("Duration of the focus session (in minutes): ");
+        let customized_focus_session_minutes = handle_number_input();
+        println!("Duration of the short break session (in minutes): ");
+        let customized_short_break_session_minutes = handle_number_input();
+        println!("Duration of the long break session (in minutes): ");
+        let customized_long_break_session_minutes = handle_number_input();
+        println!("How many sections before taking the long break: ");
+        let customized_number_of_sections_before_long_break = handle_number_input();
+        
+        customized_session_loop(
+            customized_focus_session_minutes, 
+            customized_short_break_session_minutes,
+            customized_long_break_session_minutes,
+            customized_number_of_sections_before_long_break,
+        );
+    }
+
     println!();
 }
